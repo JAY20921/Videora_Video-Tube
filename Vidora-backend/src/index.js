bdc19634f5ae8c57bf4ecd1456e310ed624fc60d
JAY20921 <jaylade012@gmail.com>
2025-12-30 19:54:21 +0530

    fixed index.js

--- Vidora-backend/src/index.js
+++ Vidora-backend/src/index.js
@@ -1,21 +1,23 @@
 // require('dotenv').config({path: './env'});
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import {app} from './app.js';
-dotenv.config({
-    path: './.env'
-});
+import { app } from "./app.js";
 
+dotenv.config();
 
 connectDB()
- .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running on port ${process.env.PORT || 8000}`);
-    })
- })
- .catch((error) => {
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
     console.error("Failed to connect to the database:", error);
- });
+    process.exit(1);
+  });
+
 
 
 
