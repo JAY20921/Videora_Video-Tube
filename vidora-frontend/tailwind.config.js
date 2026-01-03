e4a7ce95557fb502ecd44c3c8246c4369c08fe9c
JAY20921 <jaylade012@gmail.com>
2026-01-03 16:27:06 +0530

    Seperated frontend

---
+++ vidora-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./context/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
